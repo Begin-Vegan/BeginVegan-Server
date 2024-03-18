0a7fb4168d19c6c7a3699cc12c55578b45c87e65
강승우 <phonil@naver.com>
2024-03-18 18:09:20 +0900

    [CHORE]: swagger hidden

--- src/main/java/com/beginvegan/global/nginx/PingController.java
+++ src/main/java/com/beginvegan/global/nginx/PingController.java
@@ -1,10 +1,12 @@
 package com.beginvegan.global.nginx;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Hidden
 @RequiredArgsConstructor
 @Slf4j
 @RestController
